8e7bc2323d153c935fa550241345a42fb26f010a
Bryan Duplantis <duplantis@gmail.com>
2015-07-21 11:35:30 -0500

    running filter, math tests using mocha + chai

---
+++ test/test.js
@@ -0,0 +1,78 @@
+var path = require('path');
+var should = require("chai").should();
+var Animal = require(path.join(process.cwd() + '/lib/animal'));
+
+describe('Mocha + Chai', function () {
+  it('truthyness', function () {
+    true.should.equal(true);
+    false.should.be.false;
+  });
+});
+
+describe('animal', function () {
+  describe('constructor', function () {
+    it('should return an animal object', function () {
+      var animal = new Animal();
+
+      animal.should.be.an('object');
+      animal.should.be.an.instanceOf(Animal);
+    });
+    it('should be alive', function () {
+      var animal = new Animal();
+
+      animal.isAlive.should.be.true;
+    });
+  });
+
+  describe('#beCute()', function () {
+    it('should make the animal cute', function () {
+      var animal = new Animal();
+      animal.isCute.should.not.be.true;
+      animal.beCute();
+      animal.isCute.should.be.true;
+    });
+  });
+});
+
+describe('Array', function () {
+  describe('#filter()', function () {
+    it('should return an array of items that return truthy in the inner fn', function () {
+      var array  = [1,2,3,4,5];
+
+      var output = array.filter(function (item) {
+        return item % 2;
+    });
+
+      output.should.eql([1,3,5]);
+    });
+  });
+
+
+  describe('#map()', function () {
+    it('should keep the same length', function () {
+      var array = [1,2,3,4,5];
+
+      var length = array.map(function (item) {
+        return false;
+
+      }).length;
+
+      length.should.eql(array.length);
+    });
+
+    it('should not mutate the original array', function () {
+      var array = [1,2,3,4,5];
+
+      array.map(function (item) {
+        return false;
+      });
+
+      array.should.eql([1,2,3,4,5]);
+    });
+
+  });
+});
+
+
+
+// # is an instance method; runs on an instance of an array
